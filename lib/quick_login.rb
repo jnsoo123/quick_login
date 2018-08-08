60203bd71d450ac5d4101ae404edd750861bc111
jnsoo123 <neil.soo@adish.co.jp>
2018-08-08 13:49:20 +0800

    added devise gem in test dummy app

--- lib/quick_login.rb
+++ lib/quick_login.rb
@@ -2,10 +2,15 @@ require "quick_login/engine"
 
 module QuickLogin
   def quick_login_table(users, show_fields: [])
+    devise_fields = [:encrypted_password, :reset_password_token, 
+                     :reset_password_sent_at, :remember_created_at, 
+                     :sign_in_count, :current_sign_in_ip, :last_sign_in_ip,
+                     :current_sign_in_at, :last_sign_in_at]
+
     return nil unless Rails.env.development?
 
     klass = users.first.class
-    columns = klass.columns.map(&:name).map(&:to_sym) - [:created_at, :updated_at]
+    columns = klass.columns.map(&:name).map(&:to_sym) - [:created_at, :updated_at] - devise_fields
     columns = show_fields.presence || columns
 
     thead = content_tag :thead do
--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   mount QuickLogin::Engine => "/quick_login"
 
   root to: 'home#index'
