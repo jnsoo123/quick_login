26fecafdd7ee5c6e51afb7608e61b55ee2600864
jnsoo123 <neil.soo@adish.co.jp>
2018-08-09 09:22:06 +0800

    made showing of attributes dynamic

--- lib/quick_login.rb
+++ lib/quick_login.rb
@@ -3,8 +3,33 @@ require "quick_login/engine"
 module QuickLogin
   def quick_login_table(model, show_fields: [])
     return nil unless Rails.env.development?
-    return nil unless model.respond_to? :devise_modules
 
+    valid_model? model
+
+    fields     = set_fields(model, show_fields)
+    collection = model.all
+
+    thead = set_thead(fields)
+    tbody = set_tbody(collection, fields)
+    table = content_tag(:table, thead.concat(tbody), class: 'table table-bordered')
+    title = content_tag(:h3, 'Quick Login - This should only be present in Development environment')
+
+    content_tag(:div, title.concat(table), style: 'display: inline-block;')
+  end
+
+  private
+
+  def valid_model?(model)
+    raise ArgumentError, "#{model} doesn't have devise installed." unless model.respond_to? :devise_modules
+  end
+
+  def valid_attribute?(resource, column)
+    return true if column == :action
+
+    raise ArgumentError, "#{column} is not an attribute of #{resource.class}." unless resource.respond_to? column
+  end
+
+  def set_fields(model, show_fields)
     klass_fields     = model.columns.map(&:name).map(&:to_sym)
     timestamp_fields = [:created_at, :updated_at]
     devise_fields    = [:encrypted_password, :reset_password_token,
@@ -12,36 +37,41 @@ module QuickLogin
                         :sign_in_count, :current_sign_in_ip, :last_sign_in_ip,
                         :current_sign_in_at, :last_sign_in_at]
 
-    columns = klass_fields - timestamp_fields - devise_fields
-    columns = show_fields.presence || columns
-    columns = columns + [:action]
+    fields = klass_fields - timestamp_fields - devise_fields
+    fields = show_fields.presence || fields
+    fields + [:action]
+  end
 
-    users = model.all
-
-    thead = content_tag :thead do
-      columns.collect do |column|
-        concat content_tag(:th, column.capitalize)
+  def set_thead(fields)
+    content_tag :thead do
+      fields.collect do |field|
+        concat content_tag(:th, field.capitalize)
       end.join.html_safe
     end
+  end
 
-    tbody = content_tag :tbody do
-      users.collect do |user|
+  def set_tbody(collection, fields)
+    content_tag :tbody do
+      collection.collect do |resource|
         content_tag :tr do
-          td_columns = columns.collect do |column|
+          td_columns = fields.collect do |column|
+            valid_attribute?(resource, column)
+
             if column == :action
-              user_params = { user: { email: user.email, password: 'password' } }
-              concat content_tag(:td, button_to('Login', new_user_session_path, params: user_params))
+              params = { "#{resource.class.to_s.underscore}" => { email: resource.email, password: 'password' } }
+              concat content_tag(:td, button_to('Login', new_user_session_path, params: params))
+            elsif column.to_s.include? '_id'
+              value = resource.send(column.to_s.gsub('_id', '')).try(:name).presence || resource.send(column)
+              concat content_tag(:td, value)
+            elsif resource.send(column).respond_to? :attributes
+              value = resource.send(column).try(:name).presence || resource.send(column)
+              concat content_tag(:td, value)
             else
-              concat content_tag(:td, user.send(column))
+              concat content_tag(:td, resource.send(column))
             end
           end.join.to_s.html_safe
         end
       end.join.html_safe
     end
-
-    table = content_tag(:table, thead.concat(tbody), class: 'table table-bordered')
-    title = content_tag(:h3, 'Quick Login - This should only be present in Development environment')
-
-    content_tag(:div, title.concat(table), style: 'display: inline-block;')
   end
 end
